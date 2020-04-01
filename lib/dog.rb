d1074cdeb58cf42307436c45460ac66f5e61cd72
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-01 23:22:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,10 @@
 class Dog
 
   def initialize(name,breed="Mutt")
-    attr_writer: name, breed
-    attr_reader: name, breed
+    attr_writer: name
+    attr_writer: breed
+    attr_reader: name
+    attr_reader: breed
 
     @name
     @breed
